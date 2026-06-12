7f60e51c5843a4a53ae5266b71c3a8b6544c8eea
cglina <linalivashop@gmail.com>
2026-06-12 15:31:13 +0200

    gitignore correction

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-export * from "./basicTypes.js";
-export * from "./tools/arrayTools.js";
-export * from "./tools/numberTools.js";
-export * from "./tools/stringTools.js";
-export * from "./tools/objectTools.js";
-export * from "./tools/dataTools.js";
